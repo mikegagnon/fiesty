aa513c4efff381880eef0fad9cb837127763bf3f
Michael Gagnon <mikegagnon@gmail.com>
2017-01-02 22:14:55 -0500

    random pets

--- fiesty.js
+++ fiesty.js
@@ -1,4 +1,14 @@
 
+var delay_new_pet_min = 200;
+var delay_new_pet_max = 200;
+
+var delay_hide_pet_min = 1000;
+var delay_hide_pet_max = 2000;
+
+var EMPTY = 0;
+var HAPPY = "happy.png";
+var ANGRY = "angry.png";
+
 var numRows = 4
 var numCols = 4
 
@@ -10,7 +20,7 @@ var matrix = [
 ];
 
 function getLightId(row, col) {
-    return "#light-" + row + "-" + col
+    return "#light-" + row + "-" + col;
 }
 
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -22,6 +32,47 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function getPetId(row, col) {
+    return "pet-" + row + "-" + col;
+}
+
+function drawPet(row, col, pet ) {
+    var tag = "<img id='" + getPetId(row, col) + "' src='" + pet + "' height='100' width='100' >";
+    $(getLightId(row, col)).append(tag);
+}
+
+
 function lightClick(row, col) {
     alert(row + ", " + col);
 }
+
+function new_pet() {
+
+    var row = getRandomInt(0, numRows);
+    var col = getRandomInt(0, numCols);
+
+    if (matrix[row][col] == EMPTY) {
+        if (getRandomInt(0, 2) == 0) {
+            matrix[row][col] = HAPPY;
+        } else {
+            matrix[row][col] = ANGRY;
+        }
+
+        drawPet(row, col, matrix[row][col]);
+
+    }
+
+    setTimeout(new_pet, getRandomInt(delay_new_pet_min, delay_new_pet_max));
+
+
+    function rm_pet() {
+        matrix[row][col] = EMPTY;
+        $("#" + getPetId(row, col)).remove();
+    }
+
+    setTimeout(rm_pet, getRandomInt(delay_hide_pet_min, delay_hide_pet_max));
+
+}
+
+setTimeout(new_pet, getRandomInt(delay_new_pet_min, delay_new_pet_max));
+
