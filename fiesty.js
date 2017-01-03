5fe6dfa569bb20275e32dbd4c21a078e355b300a
Michael Gagnon <mikegagnon@gmail.com>
2017-01-02 21:59:12 -0500

    init

---
+++ fiesty.js
@@ -0,0 +1,27 @@
+
+var numRows = 4
+var numCols = 4
+
+var matrix = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+];
+
+function getLightId(row, col) {
+    return "#light-" + row + "-" + col
+}
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function lightClick(row, col) {
+    alert(row + ", " + col);
+}
